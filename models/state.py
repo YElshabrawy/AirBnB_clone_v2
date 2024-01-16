03e652470105bc5a3b46ca952dc6d532eb9a3fc2
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-01-16 21:39:04 +0200

    update state city

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import environ
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    storageType = environ.get("HBNB_TYPE_STORAGE", "")
+    if storageType == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            '''Get all cities'''
+            from models.city import City
+            from models import storage
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
