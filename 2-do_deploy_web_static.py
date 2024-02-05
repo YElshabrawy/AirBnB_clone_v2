07c6db17b2333771745ec5333771c721edaa2e62
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-02-05 22:18:25 +0200

    ..

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,32 +10,37 @@ env.hosts = ['54.236.207.221', '3.89.146.24']
 
 @task
 def do_pack():
-    '''Function to generate a .tgz file'''
-    local("mkdir -p versions")
-    filename = "versions/web_static_{}.tgz" \
-        .format(datetime.now().strftime("%Y%m%d%H%M%S"))
-    result = local("tar -cvzf {} web_static".format(filename))
-    if result.failed:
+    """"""
+    try:
+        cur_date = datetime.now().strftime('%Y%m%d%H%M%S')
+        arch = f'web_static_{cur_date}.tgz'
+        local('mkdir -p versions')
+        local(f'tar -cvzf versions/{arch} web_static')
+        return f'versions/{arch}'
+    except Exception:
         return None
-    else:
-        return filename
 
 
+@task
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-    file_name = archive_path.split('/')[-1].split('.')[0]
-    if not os.path.exists(archive_path):
+    '''deploy to web server'''
+    if not os.path.isfile(archive_path):
         return False
 
-    put(archive_path, '/tmp/')
-    run(f'mkdir -p /data/web_static/releases/{file_name}/')
-    run(f'tar -xzf /tmp/{file_name}.tgz -C /data/web_static/releases/{file_name}/')
-    run(f'rm /tmp/{file_name}.tgz')
-    run(f'mv /data/web_static/releases/{file_name}/web_static/* \
-    /data/web_static/releases/{file_name}/')
-    run(f'rm -rf /data/web_static/releases/{file_name}/web_static')
-    run('rm -rf /data/web_static/current')
-    run(f'ln -s /data/web_static/releases/{file_name}/ \
-    /data/web_static/current')
-    print('New version deployed!')
-    return True
+    try:
+        put(archive_path, '/tmp/')
+        archive_name = os.path.basename(archive_path)
+        folder_name = archive_name.split('.')[0]
+        release_path = "/data/web_static/releases/{}/".format(folder_name)
+
+        run(f'mkdir -p {release_path}')
+        run(f'tar -xzf /tmp/{archive_name} -C {release_path}')
+        run(f'rm /tmp/{archive_name}')
+        run(f'mv {release_path}web_static/* {release_path}')
+        run(f'rm -rf {release_path}web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s -f {release_path} /data/web_static/current')
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
