7a445f3a24b527e82b8b4480f5d0760b87ee188f
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-18 21:55:31 +0200

    ..

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Module that starts a Flask web application '''
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    ''' Function that generates a page with a message '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Function that generates a page with a message '''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def html(text):
+    '''Function to return text'''
+    return f'C {escape(text.replace("_", " "))}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
